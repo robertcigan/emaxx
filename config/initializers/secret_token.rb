7c65b1ad5b40bb8bc79ccb650aab0b4e9912fd68
Robert Cigan <cigan.robert@gmail.com>
2011-05-28 12:21:57 +0200

    initial commit of a newly generated rails app based od 3.1rc1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emaxx::Application.config.secret_token = '53e3939da513f1bf2ea0d393e7b1ac11094b6051e877929f67b93355942ad73c0593990dfb30e981cf7ec30a8d000cdf2454562b34be91dc71b1320746239516'
